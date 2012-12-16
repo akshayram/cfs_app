13663ac66ff0ad2cf30795cf446a382fcfa00cb8
Akshay Ram <akshayram@berkeley.edu>
2012-12-15 16:54:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CfsApp::Application.config.secret_token = '952364844101ed1e338bfb26c25ed35f836413ca6c00249c14a18298bfcb0e86486a9d816cbae6cdd6762bdeab621ac8b32faa61e4cc9bb9b59512fb85b28521'
